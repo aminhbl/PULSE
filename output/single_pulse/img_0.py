c775de2c41450ac760dc40fd7d14c0c9fa079d4e
Amin Habibollah <amin.habibllh@gmail.com>
2025-04-11 23:08:38 -0600

    run example

--- output/single_pulse/img_0.py
+++ output/single_pulse/img_0.py
@@ -1,61 +1,33 @@
 import turtle
 
-def draw_spiral():
+def draw_square_spiral():
+    # Setup the screen
+    screen = turtle.Screen()
+    screen.bgcolor("white")
+    
     # Setup the turtle
-    turtle.speed(0)
-    turtle.bgcolor("white")
-    turtle.pencolor("black")
-    turtle.penup()
+    spiral_turtle = turtle.Turtle()
+    spiral_turtle.color("black")
+    spiral_turtle.speed(0)  # Fastest speed
+    spiral_turtle.penup()
+    spiral_turtle.goto(0, 0)
+    spiral_turtle.pendown()
     
-    # Start at the center of the canvas
-    turtle.goto(0, 0)
-    turtle.pendown()
+    # Draw the square spiral
+    step = 10
+    for i in range(1, 21):  # Adjust the range for more or fewer turns
+        spiral_turtle.forward(step * i)
+        spiral_turtle.right(90)
     
-    # Initial square size
-    size = 20
-    
-    # Draw the initial small square
-    for _ in range(4):
-        turtle.forward(size)
-        turtle.right(90)
-    
-    # Move to the bottom-right corner of the initial square
-    turtle.penup()
-    turtle.goto(size, -size)
-    turtle.pendown()
-    
-    # Draw the spiral
-    while True:
-        # Extend the line to the right
-        size += 20
-        turtle.forward(size)
-        
-        # Extend the line upward
-        size += 20
-        turtle.left(90)
-        turtle.forward(size)
-        
-        # Extend the line to the left
-        size += 20
-        turtle.left(90)
-        turtle.forward(size)
-        
-        # Extend the line downward
-        size += 20
-        turtle.left(90)
-        turtle.forward(size)
-        
-        # Check if the spiral has reached the top-left corner
-        if turtle.xcor() <= -turtle.window_width() // 2 or turtle.ycor() >= turtle.window_height() // 2:
-            break
-    
-    # Draw the final vertical line downward
-    turtle.left(90)
-    turtle.forward(turtle.window_height())
+    # Draw the vertical line from the top-left corner
+    spiral_turtle.penup()
+    spiral_turtle.goto(-step * 10, step * 10)
+    spiral_turtle.setheading(270)  # Point downwards
+    spiral_turtle.pendown()
+    spiral_turtle.forward(step * 20)
     
     # Hide the turtle and finish
-    turtle.hideturtle()
-    turtle.done()
+    spiral_turtle.hideturtle()
+    screen.mainloop()
 
-# Run the program
-draw_spiral()
+draw_square_spiral()
